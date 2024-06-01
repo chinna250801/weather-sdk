d4e40cd793bad38a2100a5ee5a34408753bd09a7
N_V_NAGENDRA_REDDT <nagendra.r@wingage.co>
2024-06-01 12:15:52 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name="weather-sdk",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
+setup(
+    name="weather_sdk",
+    version="0.1.0",
     author="Shin Chan",
-    author_email="your.email@example.com",
-    description="A Python SDK for accessing weather data from OpenWeatherMap API",
-    long_description="",
-    long_description_content_type="text/markdown",
+    author_email="doraemon.email@example.com",
+    description="A Python SDK for interacting with the OpenWeatherMap API",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url="https://github.com/china250801/weather-sdk",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
+    include_package_data=True,
 )
